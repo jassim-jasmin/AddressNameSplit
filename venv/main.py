9aadb66a3bcc47f8353fe3333d7f5da8911115e5
jassim <incubator482@gmail.com>
2019-06-04 14:30:07 +0530

    json file made reuseble

--- venv/main.py
+++ venv/main.py
@@ -10,7 +10,10 @@ def extract(schemaName, tableName, idColumn, fieldName, outFiledName,nameOrAddre
     obj = sqlDB()
     name = jsonData[nameOrAddress]
     for pattern in name:
-        obj.zillowUpdate(schemaName, tableName, idColumn, fieldName, outFiledName, 'where ' + pattern['where'],
+        obj.zillowUpdate(schemaName, tableName, idColumn, fieldName, outFiledName, 'where ' + fieldName + " "+ pattern['where'] + " and " + outFiledName + " is null",
                          pattern['pattern'], pattern['group'])
 
+start = time.time()
 extract('testj', 'pe_owner', 'id', 'ADDRESS1', 'address_extract', 'address')
+end = time.time()
+print("Complete execution time: ", end-start)
